931e25de343d66aee31ca80d8cc976669094ce3e
PandaRider <douheng@gmail.com>
2018-01-30 21:15:12 +0800

    init 2

--- src/TestCalendar.js
+++ src/TestCalendar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import events from './events';
@@ -7,18 +6,17 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
 
-let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
-
-let Basic = props => (
+let Timeslots = props => (
   <BigCalendar
     {...props}
     events={events}
-    views={allViews}
-    step={60}
-    defaultDate={new Date(2015, 3, 1)}
-    startAccessor='startDate'
-    endAccessor='endDate'
+    step={30}       // defaults t
+    timeslots={1}
+    min={new Date(2018, 0, 30, 8, 0, 0)}
+    max={new Date(2018, 0, 30, 18, 0, 0)}
+    defaultView="week"
+    defaultDate={new Date(2018, 0, 30)}
   />
 )
 
-export default Basic
+export default Timeslots;
